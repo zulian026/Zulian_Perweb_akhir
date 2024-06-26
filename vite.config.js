cd1da745df988a7135b837834339c4a629965e87
zulian <zulianalhisyam@gmail.com>
2024-06-27 01:09:55 +0700

    feat: routing

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/Zulian_Perweb_akhir/",
+  //base : "/Zulian_Perweb_akhir/",
 })
