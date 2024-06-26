197ac1619f1e9f084fef0bb75ef026f852f49d0b
zulian <zulianalhisyam@gmail.com>
2024-06-27 00:58:13 +0700

    test

--- src/component/Navigasi.jsx
+++ src/component/Navigasi.jsx
@@ -10,7 +10,7 @@ function Navigasi() {
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
               <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="me-auto">
-                      <Nav.Link href="/" className="nav-item">
+                      <Nav.Link href="/Zulian_Perweb_akhir" className="nav-item">
                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house" viewBox="0 0 16 16">
                               <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
                           </svg>
